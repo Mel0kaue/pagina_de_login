a16dbd74ec756cca1582823ce5a7e02546129960
Mel0kaue <kauemf10@gmail.com>
2025-05-03 14:31:06 -0300

    Mudança na resposta da validação

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const emailImput = document.querySelector('input[name="email"]');
 const passwordInput = document.querySelector('input[name="password"]');
 const loginBtn = document.querySelector(".solid");
 
+const messageContainer = document.createElement("div");
+messageContainer.style.marginTop = "10px";
+form.appendChild(messageContainer);
+
 loginBtn.addEventListener("click", () => {
   const email = emailImput.value.trim();
   const password = passwordInput.value.trim();
 
   if (email === "" || password === "") {
-    alert("Preencha todos os campos!");
+    messageContainer.textContent = "Preencha todos os campos";
+    messageContainer.style.color = "white";
+    messageContainer.style.backgroundColor = "tomato";
+    messageContainer.style.padding = "10px";
   } else {
-    alert("Login realizado com sucesso!");
+    messageContainer.textContent = "Login realizado com sucesso!";
+    messageContainer.style.color = "white";
+    messageContainer.style.backgroundColor = "green";
+    messageContainer.style.padding = "10px";
   }
 });
